c56edb749a95c39e75699c0d845a92d0c4996a4d
kurniawaanr <pitesekur@yahoo.com>
2024-04-18 20:49:23 +0700

    initial

---
+++ src/components/Intro.jsx
@@ -0,0 +1,91 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { IconLink } from '@/components/IconLink'
+import { Logo } from '@/components/Logo'
+import { SignUpForm } from '@/components/SignUpForm'
+
+function BookIcon(props) {
+  return (
+    <svg viewBox="0 0 16 16" aria-hidden="true" fill="currentColor" {...props}>
+      <path d="M7 3.41a1 1 0 0 0-.668-.943L2.275 1.039a.987.987 0 0 0-.877.166c-.25.192-.398.493-.398.812V12.2c0 .454.296.853.725.977l3.948 1.365A1 1 0 0 0 7 13.596V3.41ZM9 13.596a1 1 0 0 0 1.327.946l3.948-1.365c.429-.124.725-.523.725-.977V2.017c0-.32-.147-.62-.398-.812a.987.987 0 0 0-.877-.166L9.668 2.467A1 1 0 0 0 9 3.41v10.186Z" />
+    </svg>
+  )
+}
+
+function GitHubIcon(props) {
+  return (
+    <svg viewBox="0 0 16 16" aria-hidden="true" fill="currentColor" {...props}>
+      <path d="M8 .198a8 8 0 0 0-8 8 7.999 7.999 0 0 0 5.47 7.59c.4.076.547-.172.547-.384 0-.19-.007-.694-.01-1.36-2.226.482-2.695-1.074-2.695-1.074-.364-.923-.89-1.17-.89-1.17-.725-.496.056-.486.056-.486.803.056 1.225.824 1.225.824.714 1.224 1.873.87 2.33.666.072-.518.278-.87.507-1.07-1.777-.2-3.644-.888-3.644-3.954 0-.873.31-1.586.823-2.146-.09-.202-.36-1.016.07-2.118 0 0 .67-.214 2.2.82a7.67 7.67 0 0 1 2-.27 7.67 7.67 0 0 1 2 .27c1.52-1.034 2.19-.82 2.19-.82.43 1.102.16 1.916.08 2.118.51.56.82 1.273.82 2.146 0 3.074-1.87 3.75-3.65 3.947.28.24.54.73.54 1.48 0 1.07-.01 1.93-.01 2.19 0 .21.14.46.55.38A7.972 7.972 0 0 0 16 8.199a8 8 0 0 0-8-8Z" />
+    </svg>
+  )
+}
+
+function FeedIcon(props) {
+  return (
+    <svg viewBox="0 0 16 16" aria-hidden="true" fill="currentColor" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2.5 3a.5.5 0 0 1 .5-.5h.5c5.523 0 10 4.477 10 10v.5a.5.5 0 0 1-.5.5h-.5a.5.5 0 0 1-.5-.5v-.5A8.5 8.5 0 0 0 3.5 4H3a.5.5 0 0 1-.5-.5V3Zm0 4.5A.5.5 0 0 1 3 7h.5A5.5 5.5 0 0 1 9 12.5v.5a.5.5 0 0 1-.5.5H8a.5.5 0 0 1-.5-.5v-.5a4 4 0 0 0-4-4H3a.5.5 0 0 1-.5-.5v-.5Zm0 5a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
+      />
+    </svg>
+  )
+}
+
+function LinkedInIcon(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><path fill="currentColor" d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248c-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586c.173-.431.568-.878 1.232-.878c.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252c-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/></svg>
+  )
+}
+
+function TwitterIcon(props) {
+  return (
+    <svg viewBox="0 0 16 16" aria-hidden="true" fill="currentColor" {...props}>
+      <path d="M5.526 13.502c5.032 0 7.784-4.168 7.784-7.783 0-.119 0-.237-.008-.353a5.566 5.566 0 0 0 1.364-1.418 5.46 5.46 0 0 1-1.571.431c.571-.342.998-.88 1.203-1.513a5.483 5.483 0 0 1-1.737.664 2.738 2.738 0 0 0-4.662 2.495 7.767 7.767 0 0 1-5.638-2.858 2.737 2.737 0 0 0 .847 3.651 2.715 2.715 0 0 1-1.242-.341v.035a2.737 2.737 0 0 0 2.195 2.681 2.73 2.73 0 0 1-1.235.047 2.739 2.739 0 0 0 2.556 1.9 5.49 5.49 0 0 1-4.049 1.133A7.744 7.744 0 0 0 5.526 13.5" />
+    </svg>
+  )
+}
+
+export function Intro() {
+  return (
+    <>
+      <div className="flex">
+        <Image src={"/profile.jpg"} alt="Kurniawan Ramdhani" width={300} height={300}
+        alt="Author" className="w-40 h-40 rounded-full" />
+      </div>
+      <h1 className="mt-8 font-display text-4xl/tight font-light text-white">
+        <span className="text-sky-300"><b>Kurniawan</b> Ramdhani</span><br />
+      </h1>
+      <h2 className="font-display text-md/tight font-regular text-white">
+        Freshman Back-End & DevOps Enthusiast
+      </h2>
+
+      <p className="my-8 text-md/5 font-light text-gray-300">
+      Hello there! 👋
+      I recently graduated with a Bachelor's degree in Informatics from Indonesia. Currently, I'm delving into web development with a focus on <b>backend</b> technologies. Additionally, I'm exploring the realm of <b>DevOps</b>, learning about various tools and experimenting with their usage.
+      </p>
+
+      <p className="mt-4 text-sm/6 text-gray-300">
+        Lorem Ipsum
+      </p>
+      <div className="mt-8 flex flex-wrap justify-center gap-x-1 gap-y-3 sm:gap-x-2 lg:justify-start">
+        <IconLink href="https://github.com/kurniawaanr" icon={GitHubIcon} className="flex-none" target="_blanks">
+          GitHub
+        </IconLink>
+        <IconLink href="https://www.linkedin.com/in/kurniawaanr" icon={LinkedInIcon} className="flex-none" target="_blanks">
+          LinkedIn
+        </IconLink>
+      </div>
+    </>
+  )
+}
+
+export function IntroFooter() {
+  return (
+    <p className="flex items-baseline gap-x-2 text-[0.8125rem]/6 text-gray-500">
+      Brought to you by{' '}
+      Kurniawan Ramdhani
+    </p>
+  )
+}
